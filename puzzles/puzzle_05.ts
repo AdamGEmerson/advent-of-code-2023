57755affe1f13c1472d9ef94d6a6ed0060f7bbc8
Adam G. Emerson <adamgemerson@gmail.com>
2023-12-04 17:35:26 -0500

    Day 3 puzzle solutions.

---
+++ puzzles/puzzle_05.ts
@@ -0,0 +1,70 @@
+// Advent of Code 2023
+// Puzzle #5
+
+// INPUT:
+const file = Bun.file("../input/input_05.txt")
+const input = await file.text()
+let lines = input.split("\n")
+lines = lines.slice(0, lines.length - 1)
+
+// SOLUTION:
+console.time('Runtime')
+
+// Get array of symbol indices
+// For each indices, check surrounding squares
+// If square contains numeric, get full number by checking until period
+// Add to sum and replace with dots
+interface Coordinate {
+  i: number,
+  j: number
+}
+
+let sum = 0
+let matrix = lines.map((line) => line.split(''))
+const isNumber = (c: string) => (parseInt(c) >= 0 && parseInt(c) <= 9)
+function hasAdjacentNums({i, j}: Coordinate) {
+  // console.log("Matrix Value: ", matrix[i][j])
+  const checks = [[i - 1, j], [i + 1, j], [i, j - 1], [i, j + 1], [i + 1, j + 1], [i + 1, j-1], [i - 1, j+ 1], [i - 1, j - 1]]
+  for (let k = 0; k < checks.length; k++) {
+    if (isNumber(matrix[checks[k][0]][checks[k][1]])) {
+      return {i: checks[k][0], j: checks[k][1]}
+    }
+  }
+  return false
+}
+
+function getNum({i, j}:Coordinate) {
+  let left = j, right = j
+  let num = matrix[i][j]
+  matrix[i][j] = '.'
+  while (isNumber(matrix[i][right + 1])) {
+    num += matrix[i][right + 1]
+    matrix[i][j + 1] = '.'
+    right++
+  }
+  while (isNumber(matrix[i][left - 1])) {
+    num = matrix[i][left - 1] + num
+    matrix[i][left - 1] = '.'
+    left--
+  }
+  return parseInt(num)
+}
+
+for (let i = 0; i < matrix.length; i++) {
+  for (let j = 0; j < matrix[i].length; j++) {
+    if (!isNumber(matrix[i][j]) && matrix[i][j] !== '.') {
+      let isAdjNum = hasAdjacentNums({i, j})
+      while(isAdjNum) {
+        sum += getNum(isAdjNum)
+        isAdjNum = hasAdjacentNums({i, j})
+      }
+    }
+  }
+}
+
+console.log(sum)
+console.timeEnd('Runtime')
+
+// OUTPUT:
+// 535078
+// [4.69ms] Runtime
